b4e6e9d82d18479150c3d5ffbf353a4d7cd02578
Zack Honey <zhoney466@gmail.com>
2023-12-11 11:26:14 -0800

    wip

--- api/middleware/checkUsernameFree.js
+++ api/middleware/checkUsernameFree.js
@@ -1,11 +1,11 @@
 // const User = require('../users/users-model')
 const db = require('../../data/dbConfig')
 
-const checkUsernameFree = async (req, res, next) => {
+async function checkUsernameFree(req, res, next)  {
     try {
-    //   const { username } = req.body;
-      const [existingUser] = await db("users").where("username", req.body.username);
-  
+      const { username } = req.body;
+      const [existingUser] = await db("users").where("username", username);
+                               
       if (!existingUser) {
         next(); // Username is not taken
       } else {
