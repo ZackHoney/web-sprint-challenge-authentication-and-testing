c0041db94db43626bb5d11784a4bf7e026452029
Zack Honey <zhoney466@gmail.com>
2023-12-10 20:32:37 -0800

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -11,13 +11,7 @@ function find() {
 
 function findBy(filter) {
     return db('users')
-        .select(
-            'id',
-            'username',
-            'password'
-        )
         .where(filter)
-        
 }
 
 function findById(id) {
