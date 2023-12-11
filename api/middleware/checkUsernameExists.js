b2cf5302e41780e1412ec030f0ca0d4d57110302
Zack Honey <zhoney466@gmail.com>
2023-12-10 20:08:50 -0800

    wip

--- api/middleware/checkUsernameExists.js
+++ api/middleware/checkUsernameExists.js
@@ -1,8 +1,8 @@
 const User = require('../users/users-model')
 
-const checkUsernameExists =  (req, res, next) => {
+async function checkUsernameExists(req, res, next)  {
     try {
-        const [user] =  User.findBy({ username: req.body.username })
+        const [user] = await User.findBy({ username: req.body.username })
         if (!user) {
             next({ status: 401, message: 'invalid credentials' })
         } else {
