a530c1d6ab2c73d1bb504a9ddaedcc286321cac6
Zack Honey <zhoney466@gmail.com>
2023-12-10 19:33:20 -0800

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -50,6 +50,7 @@ router.post('/register', checkUsernameFree,(req, res, next) => {
 router.post('/login', checkUsernameExists, (req, res, next) => {
   if(!req.body.username || !req.body.password) {
     res.json({
+      status: 401,
       message: 'username and password required'
     })
   } else if(bcrypt.compareSync(req.body.password, req.user.password)){
