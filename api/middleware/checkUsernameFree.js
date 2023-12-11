67e85593d978d59d9b6ba7161e3a6c73010d5bd5
Zack Honey <zhoney466@gmail.com>
2023-12-10 20:06:07 -0800

    wip

--- api/middleware/checkUsernameFree.js
+++ api/middleware/checkUsernameFree.js
@@ -2,7 +2,7 @@ const User = require('../users/users-model')
 
 async function checkUsernameFree(req, res, next) {
     try {
-        const [user] = await User.findBy({username: req.body.username})
+        const user = await User.findBy({username: req.body.username})
         if(!user.length) {
             next()
         }
